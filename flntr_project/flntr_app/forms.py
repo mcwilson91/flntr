8166919e9fe3aa9ac56fec0ab7abfc9f70176e1e
Roddy <35297977+2337543m@users.noreply.github.com>
2018-02-28 16:07:40 +0000

    room form for new room model

--- flntr_project/flntr_app/forms.py
+++ flntr_project/flntr_app/forms.py
@@ -6,10 +6,14 @@ from flntr_app.models import Flat, FlatImage, StudentProfile
 
 
 
-# class RoomForm(forms.ModelForm):
-#     # Owner = ?
-#     location = forms.CharField(help_text="Please enter the adress of the room")
-    
-#     class Meta:
-#         model = RoomDescription
-#         fields = ('description', 'picture')
+class FlatForm(forms.ModelForm):
+	streetAddress = forms.CharField(help_text="Please enter the adress of the room")
+	title =  forms.CharField(help_text="Please enter a brief description of the room")
+	postCode = forms.CharField()
+	numberOfRooms = forms.IntegerField()
+	description = forms.CharField(widget=forms.Textarea)
+	price = forms.DecimalField(max_digits=6, decimal_places=2)
+	
+	class Meta:
+		model = Flat
+		fields = ('streetAddress', 'title', 'postCode', 'numberOfRooms', 'description', 'price')
--- flntr_project/flntr_app/views.py
+++ flntr_project/flntr_app/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from flntr_app.models import Flat, FlatImage, StudentProfile
-# from flntr_app.forms import RoomForm, RoomDescriptionForm
+from flntr_app.forms import FlatForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -42,16 +43,16 @@ def user_login(request):
         return render(request, 'flntr/login.html')
 
 def property(request):
-    all_property_list = Room.objects.order_by('-price')
+    all_property_list = Flat.objects.order_by('-price')
     context_dict = {'allproperties': all_property_list}
     return render(request, 'flntr/property.html', context_dict)
 
 def show_property(request, property_id_slug):
     context_dict = {}
     try:
-        property = Room.objects.get(slug=property_id_slug)
+        property = Flat.objects.get(slug=property_id_slug)
         context_dict['property'] = property
-    except Room.DoesNotExist:
+    except Flat.DoesNotExist:
         context_dict['property'] = None
     return render(request, 'flntr/show_property.html', context_dict)
 
@@ -84,22 +85,27 @@ def user_logout(request):
     return render(request, 'flntr/user_logout.html')
 
 def add_room(request):
+	form = FlatForm();
 	if request.method == 'POST':
-		room_form = RoomForm(data=request.POST)
-		description_form = RoomDescriptionForm(data=request.POST)
-		if room_form.is_valid() and description_form.is_valid():
+		room_form = FlatForm(data=request.POST)
+		#description_form = RoomDescriptionForm(data=request.POST)
+		if room_form.is_valid():
+#		and description_form.is_valid():
 			room = room_form.save(commit=False)
-			room.owner = Landlord.objects.get(pk=1)
+			#room.owner = Landlord.objects.get(pk=1)
+			room.owner = User.objects.get(username="willie.mcsporran@yahoo.com")
+			
 			room.save()
-			description = description_form.save(commit=False)
-			description.room = room
-			if 'picture' in request.FILES:
-				description.picture = request.FILES['picture']
-			description.save()
+		#	description = description_form.save(commit=False)
+		#	description.room = room
+		#	if 'picture' in request.FILES:
+		#		description.picture = request.FILES['picture']
+		#	description.save()
 			return index(request)
 		else:
 			print(room_form.errors, description_form.errors)
 	else:	
-		room_form = RoomForm()
-		description_form = RoomDescriptionForm()
-	return render(request, 'flntr/add_room.html', {'room_form':room_form, 'description_form':description_form})
+		room_form = FlatForm()
+		#description_form = RoomDescriptionForm()
+	#return render(request, 'flntr/add_room.html', {'room_form':room_form, 'description_form':description_form})
+	return render(request, 'flntr/add_room.html', {'room_form':room_form})
